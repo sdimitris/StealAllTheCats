d256e76355ce268a760099a623e4b086f55a1329
d.soldatos <d.soldatos@kaizengaming.com>
2025-02-22 22:51:02 +0200

    git ignore

--- StealAllTheCats.Domain/obj/Debug/net8.0/StealAllTheCats.Domain.AssemblyInfo.cs
+++ StealAllTheCats.Domain/obj/Debug/net8.0/StealAllTheCats.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StealAllTheCats.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63025c6e97a08911d8260cdba2a8cbbae1ad2e8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StealAllTheCats.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StealAllTheCats.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
